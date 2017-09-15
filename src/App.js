53c0822ad3c7c66c3c0884fa9d9fd166fde859ed
Alan-Miller <alan.ashman@gmail.com>
2017-09-14 22:52:16 -0600

    checkout kinda works but with an error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 import './styles/main.css';
 
 class App extends Component {
+
+  onToken(amt, desc, checkoutCB) {
+    return (token => {
+      axios.post(`http://localhost:3121/stripe`,
+      {
+        desc,
+        source: token.id,
+        currency: 'USD',
+        amt: amt
+      })
+      .then(res => checkoutCB());
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,12 +25,33 @@ class App extends Component {
           <h2>Stripe</h2>
         </div>
 
-        <form action="/your server side code" method="POST">
-          <script></script>
-        </form>
+        <StripeCheckout 
+          name="Stripey"
+          description="Where's my money?"
+          amount={999}
+          token={this.onToken(999, "Where's my money?")}
+          currency="USD"
+          stripeKey="pk_test_hnF7JyUZM8nhb8nk1YSaJpuQ"
+        />
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+{/* <form action="/charge" method="POST">
+  <script>
+    src="https://checkout.stripe.com/checkout.js" 
+    class="stripe-button"
+    data-key="pk_test_hnF7JyUZM8nhb8nk1YSaJpuQ"
+    data-amount="999"
+    data-name="Stripey"
+    data-description="I do not know"
+    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+    data-locale="auto"
+    data-zip-code="true"
+  </script>
+</form> */}
--- srvr/index.js
+++ srvr/index.js
@@ -1,11 +1,57 @@
 const express = require('express'),
       cors = require('cors'),
       bodyParser = require('body-parser'),
+      stripeKeyPublishable = process.env.PUBLISHABLE_KEY,
+      stripeKeySecret = process.env.SECRET_KEY,
       {port} = require('../config'),
-      app = express();
+      app = express(),
+      stripe = require('stripe')(stripeKeySecret);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//————————————————————————————————————————————>> Stripe
+app.get('/stripeKeyPub', (req, res) => {
+  res.status(200).send({stripeKeyPublishable});
+});
+
+app.get('/stripe', (req, res) => {
+  res.status(200).send({stripeKeyPublishable});
+})
+
+app.post('/stripe', (req, res) => {
+  console.log('req.body', req.body)
+  stripe.charges.create(req.body, res => {
+    return (stripeErr, stripeRes) => {
+      if (stripeErr) res.status(500).send({ error: stripeErr });
+      else res.status(200).send({ success: stripeRes });
+    }
+  });
+});
 
 
+// app.get('/', (req, res) => {
+//   res.render('index.pug', {stripeKeyPublishable});
+// });
 
-app.listen(() => {
+// app.post('/charge', (req, res) => {
+//   let amount = 500;
+
+//   stripe.customers.create({
+//     email: req.body.stripeEmail,
+//     source: req.body.stripeToken
+//   })
+//   .then(customer => {
+//     stripe.charges.create({
+//       amount, 
+//       description: 'sample charge',
+//       currency: 'usd',
+//       customer: customer.id
+//     })
+//   })
+//   .then(charge => res.render('charge.pug'));
+// })
+
+app.listen( _ => {
   console.log(`Listening on ${port}.`)
 });
