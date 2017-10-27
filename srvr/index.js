904f557943b02fba5f4ad35b9e5522613b8eccae
Alan-Miller <alan.ashman@gmail.com>
2017-10-26 20:36:56 -0600

    fixed config

--- srvr/index.js
+++ srvr/index.js
@@ -1,11 +1,11 @@
 const express = require('express'),
       cors = require('cors'),
       bodyParser = require('body-parser'),
-      {port} = require('../config'),
+      { PORT } = require('../config'),
       app = express();
 
 
 
 app.listen(() => {
-  console.log(`Listening on ${port}.`)
+  console.log(`Listening on ${PORT}.`)
 });
